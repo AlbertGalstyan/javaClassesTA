358f63843f9d89180eb4726ac3b9992cfd5e6232
albert.galstyan <albert.galstyan@vineti.com>
2022-03-19 01:31:47 +0400

    Homework_2 (not completed)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,191 +1,99 @@
 package com.company;
-import java.util.Random;
-public class Main{
+import java.util.*;
 
-
+public class Main {
 
 
     public static void main(String[] args) {
-        printCircleData(generateRandomNumber());
-        printTriangleSidesSum(generateRandomNumber());
-        checkIfFirstNumberIsBigger(generateRandomNumber());
-        checkIfSumIsDivisibleByNumber(generateRandomNumber());
-        computeSquaresAndRoot(generateRandomNumber());
-        numbersAreEqual(generateRandomNumber());
-        allNumbersAreUnique(generateRandomNumber());
-        twoNumberAreEqual(generateRandomNumber());
-        threeCharactersAreEqual();
+
+        // read name, surname, weight, height, age, gender
+        // if gender is invalid, then print invalid gender (man/women)
+        // print HELLO With 'name'
+        // print Hello with my lovely 'name' if name has 'a' letter
+        // print something if name starts with 'I'
+
+        // convert and print age from kg to pounds (1kg is 2.2 pounds)
+        // generate code -> Name(first 3 digits UPPERCASE)-Surname(first 3 digits LOWECASE)-gender(MA/WM)-Age
+        // print that code
+
+        // compute BMI
+        // 74 / (1,72)^2 ≈ 25,01
+        // print perfect/not perfect according to BMI (man -> 20-30/ women -> 19-29)
 
 
-    }
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter name");
+        String name = myObj.nextLine();
 
-    public static int[] generateRandomNumber() {
-        // Here we are generating a random number
-        final int[] i = new Random().ints(1, 100).limit(6).toArray();
-        return (i);
-    }
-
+        System.out.println("Enter your Surname");
+        String surname = myObj.nextLine();
 
 
-    public static void printCircleData(int[] i) {
-        // describe radius, compute area and perimeter (use Math.PI)
-        int radius = i[0];
-        double perimeter = 2 * Math.PI * radius;
-        double area = Math.PI * radius * radius;
-        System.out.println("Given the radius of the circle, we need to find the area and perimeter");
-        System.out.println("Radius is = " + radius);
-        System.out.println("Perimeter is = " + perimeter);
-        System.out.println("Area is = " + area);
-        System.out.println("--------------------------------------");
-
-
-    }
-
-    public static void printTriangleSidesSum(int[] i) {
-        // describes sides and compute sides sum
-        int[] nums = i;
-        int sindeOne = nums[0];
-        int sideTwo = nums[1];
-        int sideThree = nums[2];
-        int sum = sindeOne+ sideTwo+ sideThree;
-        System.out.println("Given the lengths of three side of an triangle, we need to print sides sum");
-        System.out.println("The lengths of triangle sides are " + sindeOne + " , " + sideTwo + " , "+ sideThree);
-        System.out.println("The sum of triangle sides is " + sum);
-        System.out.println("--------------------------------------");
-
-
-    }
-
-    public static void checkIfFirstNumberIsBigger(int[] i) {
-        // describe two numbers, if first number if bigger print true
-        int[] nums = i;
-        int fn = nums[0];
-        int ln = nums[1];
-        System.out.println("Here we need to compare which of gove 2 number is bigger");
-        System.out.println("first number is "+ fn);
-        System.out.println("second number is "+ ln);
-
-        if (fn > ln) {
-            System.out.println("The first number is bigger " +fn+ " > " + ln);
-        }
-        else if (fn < ln){
-            System.out.println("The second number is bigger " +fn+ " < " + ln);
-        } else {
-            System.out.println("They are equal");
-        }
-        System.out.println("--------------------------------------");
-    }
-
-    public static void checkIfSumIsDivisibleByNumber(int[] i) {
-        // describe two numbers, multiply them, if result is divisible by 12 print true
-        int num = i[0];
-        int num1 = i[1];
-        System.out.println("We shoukd check if First number is divisible by second number");
-        System.out.println("First number is "+ num);
-        System.out.println("Second number is "+ num1);
-        if (num% num1 == 0) {
-            System.out.println("First number is divisible by second number");
-
-        }else
-        {
-            System.out.println("First number is NOT divisible by second number");
-            System.out.println("--------------------------------------");
-        }
-    }
-
-    public static void computeSquaresAndRoot(int[] i) {
-        // compute square of number
-        // compute root of number
-        System.out.println("We should calculate the square and the root of number: "+ i[0]);
-        double square = Math.pow(i[0],2);
-        double root = Math.sqrt(i[0]);
-        System.out.println("The square of our number is " +square+ " and the root is "+ root);
-        System.out.println("--------------------------------------");
-
-
-    }
-
-    public static void numbersAreEqual(int[] i) {
-        // having 6 number, print true if all number are equal
-        System.out.println("Here we should compare SIX numbers and see if they are same or not");
-        int check = i[0];
-        int counter = 0;
-        System.out.println("Comparing the first number with other five ones");
-        for (int n = 1; n< i.length; n++) {
-           System.out.println(check + " -> " + i[n]);
-           if (check == i[n]) {
-               counter++;
-           }
-       }
-       if (counter == 5) {
-           System.out.println("All nums are equal");
-       }else {
-           System.out.println("Nums are not equal");
-       }
-       System.out.println("--------------------------------------");
-
-    }
-
-    public static void allNumbersAreUnique(int[] i) {
-        // having 3 number, print true if all numbers are unique
-        System.out.println("Here we should compare Three numbers and see if they are ALL unique");
-        int check = i[0];
-        int counter = 0;
-        System.out.println("Comparing the first number with other Two ones");
-        for (int n = 1; n< 3; n++) {
-            System.out.println(check + " -> " + i[n]);
-            if (check != i[n]) {
-                counter++;
+        boolean checkheight = true;
+        while (checkheight) {
+            try {
+                System.out.println("input your height");
+                double height = myObj.nextDouble();
+                System.out.println("your height is " + height);
+                checkheight = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter only numbers");
+                myObj.next();
             }
+
         }
-        if (counter == 2) {
-            System.out.println("All nums are unique");
-        }else {
-            System.out.println("Nums are not unique");
-        }
-        System.out.println("--------------------------------------");
 
-    }
-
-
-    public static void twoNumberAreEqual(int[] i) {
-        // having 3 numbers, print true if two number are equal
-        System.out.println("Here we should compare Three numbers and see if two of them are equal");
-        int check = i[0];
-        int counter = 0;
-        System.out.println("Comparing the first number with other Two ones");
-        for (int n = 1; n< 3; n++) {
-            System.out.println(check + " -> " + i[n]);
-            if (check != i[n]) {
-                counter++;
+        boolean check = true;
+        while (check) {
+            try {
+                System.out.println("input your weight (kgs)");
+                double weight = myObj.nextDouble();
+                double weightPounds = weight * 2.2;
+                System.out.println("your weight is " + weight + " kgs and " + weightPounds +" in pounds");
+                check = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter only numbers");
+                myObj.next();
             }
-        }
-        if (counter == 1) {
-            System.out.println("Two of nums are equal");
-        }else {
-            System.out.println("All numbers are unique");
-        }
-        System.out.println("--------------------------------------");
-    }
 
-    public static void threeCharactersAreEqual() {
-        // having 4 characters, print true if 3 characters are same
-        System.out.println("Here we should compare four chars and see if three of them are equal");
-        char[] cl = {'/', '/', 'v', '/'};
-        int index = 0;
-        int counter = 0;
-        System.out.println("Comparing the first char with other ones");
-        for (int i = 1; i < cl.length; i++) {
-            System.out.println(cl[index] + " -> " + cl[i]);
-            if (cl[index] == cl[i]) {
-                counter++;
+        }
+
+
+
+        boolean checkage = true;
+        while (checkage) {
+            try {
+                System.out.println("input your age");
+                int age = myObj.nextInt();
+                System.out.println("your age is " + age);
+                checkage = false;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter only numbers");
+                myObj.next();
             }
+
         }
-        if (counter == 2) {
-            System.out.println("3 chars are Equal");
-        }else {
-            System.out.println("3 chars are NOT equal");
+        Scanner myobject = new Scanner(System.in);
+        System.out.println("please enter your gender");
+        boolean gendercheck = true;
+        String gender = myobject.nextLine();
+
+        if (!gender.equals("men")){
+            System.out.println("invalid gender");
+        }else if (!gender.equals("women")){
+            System.out.println("invalid gender");
         }
+
+        System.out.println(name.toUpperCase(Locale.ROOT).substring(0,3));
+
+
+
+
+
+
+
+
 
     }
 }
+
